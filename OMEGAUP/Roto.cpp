b4f839437d59485e9f119c94e12d98554defd951
renepayan <renepayan62@gmail.com>
2017-04-16 22:35:29 -0500

    https://omegaup.com/arena/problem/roto
    https://omegaup.com/arena/problem/roto
    #colas

--- OMEGAUP/Roto.cpp
+++ OMEGAUP/Roto.cpp
@@ -9,40 +9,40 @@ int main(){
 	int sal = 0,sal2 = 0,posicion = 0;
 	bool direccion = true;
 	while(posicion<entrada.length()){
-        if(entrada[posicion] == '[' or entrada[posicion] == ']' ){
-            if(entrada[posicion]==']'){
-                direccion=true;
+        	if(entrada[posicion] == '[' or entrada[posicion] == ']' ){
+       			if(entrada[posicion]==']'){
+                		direccion=true;
 			}else{
-                direccion=false;
+                		direccion=false;
 			}
-            posicion++;
+            		posicion++;
 		}else{
-            if(direccion){
-                for(int i=posicion;posicion<entrada.length() and entrada[posicion]!='[' and entrada[posicion]!=']';i++){
-                    parte2[sal]=entrada[i];
-                    posicion++;
-                    sal++;
-                }
-            }else{
-                int d=posicion;
-                for(int i=0;d<entrada.length() and entrada[d]!='[' and entrada[d]!=']';i++){
-                    d++;
-                }
-                for(int i=d-1;i>=posicion;i--){
-                    parte1[sal2]=entrada[i];
-                    sal2++;
-                }
-                posicion=d;
-            }
-        }
-    }
+            		if(direccion){
+                		for(int i=posicion;posicion<entrada.length() and entrada[posicion]!='[' and entrada[posicion]!=']';i++){
+                    			parte2[sal]=entrada[i];
+                    			posicion++;
+                    			sal++;
+                		}
+            		}else{
+                		int d=posicion;
+                		for(int i=0;d<entrada.length() and entrada[d]!='[' and entrada[d]!=']';i++){
+                    			d++;
+                		}
+                		for(int i=d-1;i>=posicion;i--){
+                    			parte1[sal2]=entrada[i];
+                    			sal2++;
+                		}
+                		posicion=d;
+            		}
+        	}
+    	}
 	for(int i=sal2;i>=0;i--){
 		if(parte1[i]!='\000'){
-            cout<<parte1[i];
-        }
+            		cout<<parte1[i];
+        	}
 	}
 	for(int i=0;i<sal;i++){
-        cout<<parte2[i];
+        	cout<<parte2[i];
 	}
 	return 0;
 }
